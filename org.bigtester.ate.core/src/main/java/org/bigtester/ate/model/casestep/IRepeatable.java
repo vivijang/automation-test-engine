f09b7eabeca7330852d82c9eca2c9b2710dfd3e4
peidong Hu <zhouyibhic@gmail.com>
2015-03-13 10:15:23 -0700

    test

--- org.bigtester.ate.core/src/main/java/org/bigtester/ate/model/casestep/IRepeatable.java
+++ org.bigtester.ate.core/src/main/java/org/bigtester/ate/model/casestep/IRepeatable.java
@@ -39,5 +39,23 @@ public interface IRepeatable {
 	 *
 	 * @return true, if successful
 	 */
+	
+	/**
+	 * Refresh step data.
+	 *
+	 * @return true, if successful
+	 */
+	
+	/**
+	 * Refresh step data.
+	 *
+	 * @return true, if successful
+	 */
+	
+	/**
+	 * Refresh step data.
+	 *
+	 * @return true, if successful
+	 */
 	boolean refreshStepData(String repeatStepName, int iteration);
 }
